c44df94c6ba146fc82823de214ca4f204c0151ea
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-03-04 06:56:01 +0000

    devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :user, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  devise_for :admin, controllers: {
+    # 管理者登録後 skip: [:registrations, :passwords]を実装する
+    registrations: "admin/registrations",
+    sessions: "admin/sessions"
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
