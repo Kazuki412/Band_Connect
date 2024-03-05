4850ab721c212cfa452e3707e85b3ec9ec70f353
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-03-05 08:38:27 +0000

    会員登録エラー中

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, if: :devise_controller?, only: [:create]
+  
+  private
+  
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :introduction, :motivation_id])
+  end 
+  
+  def after_sign_up_path_for(resource)
+    root_path
+  end 
+  
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
--- app/models/motivation.rb
+++ app/models/motivation.rb
@@ -2,5 +2,6 @@ class Motivation < ApplicationRecord
   
   has_many :users
   
+  enum name: { professional: 0, amateur: 1, hobby: 2 }
   
 end
