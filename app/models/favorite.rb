3a0cb6d2c1c75d7c3d537f3de51b02b624197082
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-03-07 07:09:15 +0000

    いいね非同期OK

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,5 +3,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :post
   
-  validates :user_id, uniqueness: {scope: :book_id}
+  validates :user_id, uniqueness: {scope: :post_id}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ApplicationRecord
   
   belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
