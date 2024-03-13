b14a5015479d76478533044d12f94b38f9ee1025
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-03-13 08:59:23 +0000

    色々

--- app/controllers/admin/bands_controller.rb
+++ app/controllers/admin/bands_controller.rb
@@ -9,8 +9,20 @@ class Admin::BandsController < ApplicationController
   end
 
   def edit
+    @band = Band.find(params[:id])
   end
 
   def update
+    @band = Band.find(params[:id])
+    @band.update(band_params)
+    flash[:notice] = "バンドの情報を更新しました"
+    redirect_to admin_band_path(@band.id)
   end
+  
+  private
+  
+  def band_params
+    params.require(:band).permit(:band_status2)
+  end
+  
 end
--- app/controllers/public/bands_controller.rb
+++ app/controllers/public/bands_controller.rb
@@ -1,7 +1,6 @@
 class Public::BandsController < ApplicationController
-  before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update, :destroy, :band_permits]
-  
+
   def new
     @band = Band.new
   end
@@ -12,7 +11,7 @@ class Public::BandsController < ApplicationController
     @band.users << current_user
     @band.save
     flash[:notice] = "新しくバンドを作りました"
-    redirect_to public_bands_path
+    redirect_to public_band_path(@band.id)
   end
 
   def index
@@ -31,7 +30,7 @@ class Public::BandsController < ApplicationController
     @band = Band.find(params[:id])
     @band.update(band_params)
     flash[:notice] = "バンドの情報を更新しました"
-    redirect_to public_bands_path
+    redirect_to public_band_path(@band.id)
   end
 
   def destroy
@@ -40,23 +39,23 @@ class Public::BandsController < ApplicationController
     flash[:alert] = "バンドを解散しました"
     redirect_to public_bands_path
   end
-  
+
   def band_permits
     @band = Band.find(params[:id])
     @band_permits = @band.band_permits.page(params[:page])
   end
-  
+
   private
-  
+
   def band_params
-    params.require(:band).permit(:band_image, :name, :introduction, :genre_id)
+    params.require(:band).permit(:band_image, :name, :introduction, :genre_id, :band_status)
   end
-  
+
   def ensure_correct_user
     @band = Band.find(params[:id])
     unless @band.owner_id == current_user.id
       redirect_to public_bands_path, alert: "グループオーナーのみ編集が可能です"
-    end 
-  end 
+    end
+  end
 
 end
