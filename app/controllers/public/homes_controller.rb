b396cb69daa4dd6f77f0cd5167e3fb21f3c6f1a5
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-03-16 05:08:54 +0000

    publicレイアウト

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
   
   def top
+    @posts = Post.order("id DESC").limit(10)
   end
   
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -15,15 +15,16 @@ class Public::UsersController < ApplicationController
           if current.dm_room_id == another.dm_room_id
             @is_room = true
             @dm_room_id = current.dm_room_id
-          end 
-        end 
-      end 
+          end
+        end
+      end
       unless @is_room
         @dm_room = DmRoom.new
         @entry = Entry.new
-      end 
+      end
     end
     @is_same_band_member = current_user.bands.pluck(:band_id).any? {|band_id| @user.bands.pluck(:band_id).include?(band_id)}
+    @posts = @user.posts.order("id DESC").limit(10)
   end
 
   def edit
@@ -36,23 +37,23 @@ class Public::UsersController < ApplicationController
     flash[:notice] = "変更を保存しました"
     redirect_to public_user_path(@user)
   end
-  
+
   def bookmarks
     @user = User.find(params[:id])
     bookmarks = Favorite.where(user_id: @user).pluck(:post_id)
     @bookmark_posts = Post.find(bookmarks)
-  end 
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:profile_image, :name, :musical_instrument_id, :motivation_id, :introduction)
   end
-  
+
   def ensure_guest_user
     if current_user.email == "guest@guest"
       redirect_to root_path, alert: "ゲストユーザーの更新・退会はロックしています"
-    end 
-  end 
-  
+    end
+  end
+
 end
