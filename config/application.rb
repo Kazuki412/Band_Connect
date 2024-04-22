978551f3005d06dbf193ab3ff9473046778696d8
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-04-22 08:24:01 +0000

    a

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Bandconnect
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.paths.add 'lib', eager_load: true
     config.i18n.default_locale = :ja
     config.paths.add 'lib', eager_load: true
     # Configuration for the application, engines, and railties goes here.
